fe1413a03f0e4f447059f9a733156ea901a5978a
macias <bluedzins@wp.pl>
2020-10-15 18:21:53 +0200

    cleanup (NO LIVE TEST)

---
+++ app/TrackRadar/GpxLoader.Stage.cs
@@ -0,0 +1,22 @@
+﻿using Geo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrackRadar
+{
+    public partial class GpxLoader
+    {
+        private static readonly int StageCount = Enum.GetValues(typeof(Stage)).Length;
+
+        internal enum Stage
+        {
+            Loading,
+            ComputingCrossroads,
+            SplitByWaypoints,
+            AssigningTurns,
+            SectionId,
+            AlternateTurns,
+        }
+    }
+}

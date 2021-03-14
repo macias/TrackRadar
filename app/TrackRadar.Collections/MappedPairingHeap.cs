e61de4bc4359f640d93f3e13c56b7692c3e771ec
macias <bluedzins@wp.pl>
2021-03-14 20:56:46 +0100

    added comparer

--- app/TrackRadar.Collections/MappedPairingHeap.cs
+++ app/TrackRadar.Collections/MappedPairingHeap.cs
@@ -12,15 +12,16 @@ namespace TrackRadar.Collections
     public sealed class MappedPairingHeap<TWeight, TTag>
         where TWeight : IComparable<TWeight>
     {
+        // current (after updates) tag values are stored at heap, not in dict
         private readonly Dictionary<TTag, PairingHeapNode<TWeight, TTag>> dict;
         private PairingHeapNode<TWeight, TTag> root;
 
         //public (TTag tag, TWeight weight) Top => (root.Tag, root.Weight);
         public int Count => this.dict.Count;
 
-        public MappedPairingHeap()
+        public MappedPairingHeap(IEqualityComparer<TTag> comparer = null)
         {
-            this.dict = new Dictionary<TTag, PairingHeapNode<TWeight, TTag>>();
+            this.dict = new Dictionary<TTag, PairingHeapNode<TWeight, TTag>>(comparer ?? EqualityComparer<TTag>.Default);
             this.root = null;
         }
 
@@ -29,7 +30,10 @@ namespace TrackRadar.Collections
             if (this.dict.TryGetValue(tag, out PairingHeapNode<TWeight, TTag> heap_node))
             {
                 if (weight.CompareTo(heap_node.Weight) < 0)
+                {
+                    // only heap get its tag updated, but not dict
                     root.DecreaseWeight(ref root, heap_node, weight, tag);
+                }
             }
             else
             {
